f147789685f0d44d228b4dda2cba0bade4adbc1c
arvind322 <arvygurjar007@gmail.com>
2025-05-19 17:53:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,1 @@
-from flask import Flask
-from threading import Thread
-from media_filter_textbot import bot
 
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "Bot is running!"
-
-def run_flask():
-    app.run(host="0.0.0.0", port=8080)
-
-def run_bot():
-    bot.run()  # This handles event loop correctly and runs forever
-
-if __name__ == "__main__":
-    Thread(target=run_flask).start()  # Run Flask on a separate thread
-    run_bot()                         # Run bot in main thread (no asyncio.run)
