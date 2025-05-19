723f61b7a03fec4f893abe5c01a708e8b2f412fb
arvind322 <arvygurjar007@gmail.com>
2025-05-19 12:37:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
-import threading
+from threading import Thread
 import asyncio
-from media_filter_textbot import start_bot
+from media_filter_textbot import bot  # Directly import bot object
 
 app = Flask(__name__)
 
@@ -9,11 +9,13 @@ app = Flask(__name__)
 def home():
     return "Telegram Media Filter Bot is Running on Koyeb!"
 
-def run():
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    start_bot()
+def run_bot():
+    async def start():
+        await bot.start()
+        print("Bot started.")
+        await bot.idle()
+    asyncio.run(start())
 
 if __name__ == "__main__":
-    threading.Thread(target=run).start()
+    Thread(target=run_bot).start()
     app.run(host="0.0.0.0", port=8080)
