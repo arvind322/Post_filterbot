8a4279d25bdfba04b62141451b2bac0bdddea408
arvind322 <arvygurjar007@gmail.com>
2025-05-19 18:02:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
+from flask import Flask
+from threading import Thread
+from media_filter_textbot import start_bot
 
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bot is running!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
+if __name__ == "__main__":
+    keep_alive()
+    start_bot()  # This should start your bot
