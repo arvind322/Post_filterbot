8e4712208ffd6f3b6ad3d525047f221117b77827
arvind322 <arvygurjar007@gmail.com>
2025-05-19 15:45:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from flask import Flask
 import threading
-import asyncio
 from media_filter_textbot import bot
-from pyrogram.raw.functions import Ping
 
 app = Flask(__name__)
 
@@ -11,14 +9,7 @@ def home():
     return "Bot is running!"
 
 def run_bot():
-    async def main():
-        await bot.connect()
-        await bot.invoke(Ping(ping_id=0))  # टाइम सिंक के लिए जरूरी
-        await bot.start()
-        print("Bot started")
-        await asyncio.get_event_loop().create_future()  # bot को चलाता रहे
-
-    asyncio.run(main())
+    bot.run()  # Handles start, connection, and event loop
 
 if __name__ == "__main__":
     threading.Thread(target=run_bot).start()
